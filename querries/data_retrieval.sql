2526217b6e91907738385e687495589437baef5c
aldoglory <152659496+aldoglory@users.noreply.github.com>
2025-12-09 16:27:22 +0200

    Update data_retrieval.sql

--- querries/data_retrieval.sql
+++ querries/data_retrieval.sql
@@ -1,1 +1,12 @@
+-- All medications taken by a patient
+SELECT pm.intake_date, m.med_name, pm.status
+FROM patient_medications pm
+JOIN medications m ON pm.med_id = m.med_id
+WHERE pm.patient_id = 1;
+
+-- Patients with skipped medications
+SELECT DISTINCT p.patient_id, p.first_name, p.last_name
+FROM patient_medications pm
+JOIN patients p ON pm.patient_id = p.patient_id
+WHERE pm.status = 'skipped';
 
